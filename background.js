15d22a1fb9a157e64bca647e5f9872c0fff085be
leesk1283-gitHub <leesk1283-gitHub@users.noreply.github.com>
2025-12-03 05:37:57 +0000

    deploy: cadf698f7b0877b8a34b6c3865bb6b81543a61c1

---
+++ background.js
@@ -0,0 +1,6 @@
+// Background service worker for OOXML/ODF Viewer
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({
+        url: chrome.runtime.getURL('index.html')
+    });
+});
