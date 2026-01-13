11df2d5c1453eb4a7f963f5d6c5663944c27863f
leesk1283 <116324537+leesk1283-gitHub@users.noreply.github.com>
2026-01-13 17:52:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [],
-  base: mode === 'extension' ? './' : '/ooxml-odf-viewer/',
+  base: mode === 'extension' ? './' : '/',
 }))
